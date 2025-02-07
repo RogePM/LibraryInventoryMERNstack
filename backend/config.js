6bd047cdab7a1129d2aadc647053e0bb4d1463fd
Rogelio Perez-Montero <131227318+RogePM@users.noreply.github.com>
2025-02-07 15:21:29 -0500

    new changes to backend

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
-export const PORT = 5556;
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://<rperezmontero>:<The12258332@>@bookstore-mern.kuwkj.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore-MERN';
