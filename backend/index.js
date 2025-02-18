579c260181389f86aabf2fb84c8d4eab6aa0e24f
Rogelio Perez-Montero <131227318+RogePM@users.noreply.github.com>
2025-02-17 22:49:34 -0500

    front end created

--- backend/index.js
+++ backend/index.js
@@ -1,42 +1,42 @@
 import express from 'express';
-import { PORT , mongoDBURL } from './config.js';
+import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
 import bookRoute from './routes/bookRoute.js';
+import cors from 'cors';
 import { Book } from './models/bookmodel.js';
 
 const app = express();
 //middleware for parsing json data
-app.use(express.json()); 
+app.use(express.json());
 
 //middleware for handling CORS Policy 
 //Option 1: allow origins with default of cors(*)
 app.use(cors());
 //Option 2: allow origins with specific origins
-app.use(
-    corse({
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type'],
-    })
-
-)
+//app.use(
+//    cors({
+//        origin: 'http://localhost:3000',
+//        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//        allowedHeaders: ['Content-Type'],
+//    })
+//);
 
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send('Welcome!');
-}); 
+});
 
 app.use('/books', bookRoute);
 
 mongoose
-.connect (mongoDBURL) 
-.then(() => {
-    console.log('App is connected to MongoDB');
-    app.listen(PORT, () => {
-        console.log(`App is listening on port ${PORT}`);
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App is connected to MongoDB');
+        app.listen(PORT, () => {
+            console.log(`App is listening on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log('THeres an error', error);
     });
-})
-.catch((error) => {
-    console.log('THeres an error',error);
-});
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import {Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import CreateBooks from './pages/CreateBooks';
+import EditBook from './pages/EditBook';
+import DeleteBook from './pages/DeleteBook';
+import showBook from './pages/showBook';
 
 const App = () => {
   return (
-    <div className = 'bg-red'>App 
-      
-    </div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/books/create" element={<CreateBooks />} />
+      <Route path="/books/edit/:id" element={<showBook />} />
+      <Route path="/books/delete/:id" element={<DeleteBook />} />
+    </Routes> 
+  );
+};
 
 export default App
